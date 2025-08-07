fc037f7e091689885280fd468c287da9dd44daa2
d27pomaramirez <d27poma.ramirez@gmail.com>
2025-08-07 17:39:00 -0500

    creando un array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Declaración de variables
+
+let amigos = []; //lista para almacenar los nombres
+
+
+//Funciones
