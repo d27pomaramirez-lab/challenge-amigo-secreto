064cf815ee6f300bc6da26fecbb0923f9e8bdd59
d27pomaramirez <d27poma.ramirez@gmail.com>
2025-08-07 18:19:47 -0500

    agregando funcion agregar y limpiar entrada con validaciones

--- app.js
+++ app.js
@@ -4,5 +4,22 @@
 
 let amigos = []; //lista para almacenar los nombres
 
-
 //Funciones
+function agregarAmigo() {
+    let nombreDelAmigo = document.getElementById('amigo').value; //capturamos el valor ingresado en el input
+    
+    //Validar que el campo no este vacío
+    if(nombreDelAmigo === ''){
+        //mensaje de alerta
+        alert('Por favor, inserte un nombre');
+    }else{
+        //si no esta vacío agrega el nombre a la lista (actualizar la lista)
+        amigos.push(nombreDelAmigo);
+        limpiarEntrada(); //Limpiamos la entrada
+        //console.log(amigos);
+    }
+}
+
+function limpiarEntrada() {
+    document.querySelector('#amigo').value = '';
+}
